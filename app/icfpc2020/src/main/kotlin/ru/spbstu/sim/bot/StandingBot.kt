66c62b9f9b0c0e71ba1e1c2cd0a2d3cdc7b0c890
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2020-07-19 19:56:16 +0300

    fixed standing bot

--- app/icfpc2020/src/main/kotlin/ru/spbstu/sim/bot/StandingBot.kt
+++ app/icfpc2020/src/main/kotlin/ru/spbstu/sim/bot/StandingBot.kt
@@ -1,5 +1,6 @@
 package ru.spbstu.sim.bot
 
+import ru.spbstu.sim.Coordinates
 import ru.spbstu.sim.MapState
 import ru.spbstu.sim.ShipCommand
 import ru.spbstu.sim.ShipState
@@ -8,11 +9,16 @@ import ru.spbstu.sim.ShipState
 class StandingBot : AbstractBot() {
     override fun initialShipState(mapState: MapState) = ShipState(300, 0, 8, 1)
 
+    val Coordinates.randomVector: Coordinates get() {
+        val reversed = this.swap()
+        val random = listOf(-1, 1).random()
+        return Coordinates(reversed.x * random, reversed.y * random)
+    }
+
     init {
         step { ship, _, _ ->
             val velocity = gravity(ship.position)
-            val reversedGravity = gravity(ship.position.swap())
-            listOf(ShipCommand.Accelerate(ship.id, velocity + reversedGravity))
+            listOf(ShipCommand.Accelerate(ship.id, velocity + velocity.randomVector))
         }
     }
 }

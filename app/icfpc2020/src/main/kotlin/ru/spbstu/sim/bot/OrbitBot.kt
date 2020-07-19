44289573e03b530d8835d01602057e26c0ec788f
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2020-07-19 22:22:29 +0300

    v6

--- app/icfpc2020/src/main/kotlin/ru/spbstu/sim/bot/OrbitBot.kt
+++ app/icfpc2020/src/main/kotlin/ru/spbstu/sim/bot/OrbitBot.kt
@@ -11,29 +11,34 @@ class OrbitBot : AbstractBot() {
 
     init {
         step { ship, gameState, mapState ->
+            val position = ship.position
+            val planetRadius = mapState.planeRadius
             val gravity = gravity(ship.position)
             var acceleration = Coordinates(0, 0)
             val truncVelocity = ship.velocity * gravity
-            val isAbovePlanet = isAbovePlanet(ship.position, mapState.planeRadius)
-            val isCompensateX = isAbovePlanet && truncVelocity.x < ACCELERATION
-            val isCompensateY = isAbovePlanet && truncVelocity.y < ACCELERATION
+            val isCompensateX = abs(position.x) < planetRadius && truncVelocity.x < ACCELERATION
+            val isCompensateY = abs(position.y) < planetRadius && truncVelocity.y < ACCELERATION
+            val isCompensateXY = abs(position.x) == abs(position.y) &&
+                    (truncVelocity.x < ACCELERATION && truncVelocity.y < ACCELERATION)
             when {
-                isCompensateX -> acceleration += Coordinates(-gravity.x, 0)
+                isCompensateX || isCompensateXY -> acceleration += Coordinates(gravity.x, 0)
                 abs(truncVelocity.x) < 2 * ACCELERATION -> acceleration += Coordinates(gravity.x, 0)
                 abs(truncVelocity.x) > 5 * ACCELERATION -> acceleration += Coordinates(-gravity.x, 0)
             }
             when {
-                !isCompensateX && isCompensateY -> acceleration += Coordinates(0, -gravity.y)
+                isCompensateY -> acceleration += Coordinates(0, gravity.y)
                 abs(truncVelocity.y) < 2 * ACCELERATION -> acceleration += Coordinates(0, gravity.y)
                 abs(truncVelocity.y) > 5 * ACCELERATION -> acceleration += Coordinates(0, -gravity.y)
             }
+
+            println("---------------------------")
             println("tick=${gameState.tick} " +
                     "velocity=${ship.velocity} " +
                     "truncVelocity=$truncVelocity " +
                     "gravity=$gravity" +
                     "isCompensateX=$isCompensateX " +
                     "isCompensateY=$isCompensateY " +
-                    "isAbovePlanet=$isAbovePlanet ")
+                    "isCompensateXY=$isCompensateXY ")
 
             when {
                 acceleration.isZero() -> emptyList()

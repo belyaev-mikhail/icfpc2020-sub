9da3166beaed836884e605b1fe95939d9328a616
saloed <napster_1997@mail.ru>
2020-07-19 21:01:04 +0300

    185

--- app/icfpc2020/src/main/kotlin/ru/spbstu/Main.kt
+++ app/icfpc2020/src/main/kotlin/ru/spbstu/Main.kt
@@ -11,7 +11,7 @@ fun main(args: Array<String>) {
     GSMS.playerKey = playerKey
 
     val bot = CompositeBot(
-        ShipState(270, 0, 13, 1),
+        ShipState(185, 4, 20, 3),
         StandingBot()
     )
     val game = Game(bot)

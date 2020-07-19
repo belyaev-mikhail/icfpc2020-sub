270307b128042d576a8fdefcff8fc74a7bc95757
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2020-07-19 21:10:14 +0300

    v1

--- app/icfpc2020/src/main/kotlin/ru/spbstu/Main.kt
+++ app/icfpc2020/src/main/kotlin/ru/spbstu/Main.kt
@@ -3,18 +3,15 @@ package ru.spbstu
 import ru.spbstu.sim.GSMS
 import ru.spbstu.sim.Game
 import ru.spbstu.sim.ShipState
-import ru.spbstu.sim.bot.*
+import ru.spbstu.sim.bot.CompositeBot
+import ru.spbstu.sim.bot.OrbitBot
 
 fun main(args: Array<String>) {
     val (serverUrl, playerKey) = args
     GSMS.serverUrl = serverUrl
     GSMS.playerKey = playerKey
 
-    val bot = CompositeBot(
-        ShipState(256, 20, 10, 1),
-        StandingBot(),
-        ShootingBot()
-    )
+    val bot = CompositeBot(ShipState(256, 0, 15, 1), OrbitBot())
     val game = Game(bot)
     game.loop()
 }

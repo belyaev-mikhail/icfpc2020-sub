b0f9cb29a111d8fae79787c7a2a093abadb0e7c5
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2020-07-20 00:17:20 +0300

    v1-3

--- app/icfpc2020/src/main/kotlin/ru/spbstu/sim/bot/OrbitBot.kt
+++ app/icfpc2020/src/main/kotlin/ru/spbstu/sim/bot/OrbitBot.kt
@@ -25,14 +25,14 @@ class OrbitBot : AbstractBot() {
             when {
                 isCompensateX || isCompensateXY -> acceleration += Coordinates(gravity.x, 0)
                 abs(nextVelocity.x) == 0L -> acceleration += Coordinates(1, 0)
-                abs(nextVelocity.x) < 2 * ACCELERATION -> acceleration += Coordinates(-nextVelocity.x / abs(nextVelocity.x), 0)
-                abs(nextVelocity.x) > 5 * ACCELERATION -> acceleration += Coordinates(nextVelocity.x / abs(nextVelocity.x), 0)
+                abs(nextVelocity.x) < planetRadius / 2 -> acceleration += Coordinates(-nextVelocity.x / abs(nextVelocity.x), 0)
+                abs(nextVelocity.x) > planetRadius -> acceleration += Coordinates(nextVelocity.x / abs(nextVelocity.x), 0)
             }
             when {
                 isCompensateY -> acceleration += Coordinates(0, gravity.y)
                 abs(nextVelocity.y) == 0L -> acceleration += Coordinates(0, 1)
-                abs(nextVelocity.y) < 2 * ACCELERATION -> acceleration += Coordinates(0, -nextVelocity.y / abs(nextVelocity.y))
-                abs(nextVelocity.y) > 5 * ACCELERATION -> acceleration += Coordinates(0, nextVelocity.y / abs(nextVelocity.y))
+                abs(nextVelocity.y) < planetRadius / 2 -> acceleration += Coordinates(0, -nextVelocity.y / abs(nextVelocity.y))
+                abs(nextVelocity.y) > planetRadius -> acceleration += Coordinates(0, nextVelocity.y / abs(nextVelocity.y))
             }
 
             println("---------------------------")
@@ -42,6 +42,7 @@ class OrbitBot : AbstractBot() {
                     "velocity=$velocity " +
                     "nextVelocity=$nextVelocity " +
                     "truncVelocity=$truncVelocity\n" +
+                    "acceleration=$acceleration " +
                     "gravity=$gravity\n" +
                     "isCompensateX=$isCompensateX " +
                     "isCompensateY=$isCompensateY " +

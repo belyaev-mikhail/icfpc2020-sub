8c6eb972946530f75418ab45160d883a0089b1d1
saloed <napster_1997@mail.ru>
2020-07-19 16:36:12 +0300

    speed test

--- app/icfpc2020/src/main/kotlin/ru/spbstu/Main.kt
+++ app/icfpc2020/src/main/kotlin/ru/spbstu/Main.kt
@@ -5,14 +5,11 @@ import ru.spbstu.sim.bot.EmptyBot
 import java.io.File
 
 fun main(args: Array<String>) {
-    val aa = parse(File("app/icfpc2020/data/galaxy.txt").readText())
-
     val (serverUrl, playerKey) = args
-
     GSMS.serverUrl = serverUrl
     GSMS.playerKey = playerKey
 
-    val bot = EmptyBot(ShipState(216, 0, 4, 1 ))
+    val bot = EmptyBot(ShipState(400, 95, 20, 3 ))
     val game = Game(bot)
     game.loop()
 }

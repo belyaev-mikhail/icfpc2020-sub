8ff8efaa34d76314de0f0dbf0ac986b17e8a2f53
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2020-07-19 18:01:45 +0300

    shooting bot

--- app/icfpc2020/src/main/kotlin/ru/spbstu/Main.kt
+++ app/icfpc2020/src/main/kotlin/ru/spbstu/Main.kt
@@ -5,6 +5,7 @@ import ru.spbstu.sim.Game
 import ru.spbstu.sim.ShipState
 import ru.spbstu.sim.bot.CompositeBot
 import ru.spbstu.sim.bot.OrbitBot
+import ru.spbstu.sim.bot.ShootingBot
 
 fun main(args: Array<String>) {
     val (serverUrl, playerKey) = args
@@ -12,8 +13,9 @@ fun main(args: Array<String>) {
     GSMS.playerKey = playerKey
 
     val bot = CompositeBot(
-            ShipState(300, 0, 10, 1),
-            OrbitBot()
+        ShipState(300, 20, 10, 3),
+        OrbitBot(),
+        ShootingBot()
     )
     val game = Game(bot)
     game.loop()

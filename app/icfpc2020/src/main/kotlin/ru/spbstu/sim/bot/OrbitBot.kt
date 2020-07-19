c189f8f08fc237642ccd18e2cc63e4bc5f493053
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2020-07-19 23:53:09 +0300

    v1-1

--- app/icfpc2020/src/main/kotlin/ru/spbstu/sim/bot/OrbitBot.kt
+++ app/icfpc2020/src/main/kotlin/ru/spbstu/sim/bot/OrbitBot.kt
@@ -24,13 +24,15 @@ class OrbitBot : AbstractBot() {
             val isCompensateXY = abs(nextPosition.x) == abs(nextPosition.y) && (truncVelocity.x > 0 && truncVelocity.y > 0)
             when {
                 isCompensateX || isCompensateXY -> acceleration += Coordinates(gravity.x, 0)
-                abs(truncVelocity.x) < 2 * ACCELERATION -> acceleration += Coordinates(gravity.x, 0)
-                abs(truncVelocity.x) > 5 * ACCELERATION -> acceleration += Coordinates(-gravity.x, 0)
+                abs(nextVelocity.x) == 0L -> acceleration += Coordinates(1, 0)
+                abs(nextVelocity.x) < 2 * ACCELERATION -> acceleration += Coordinates(nextVelocity.x / abs(nextVelocity.x), 0)
+                abs(nextVelocity.x) > 5 * ACCELERATION -> acceleration += Coordinates(-nextVelocity.x / abs(nextVelocity.x), 0)
             }
             when {
                 isCompensateY -> acceleration += Coordinates(0, gravity.y)
-                abs(truncVelocity.y) < 2 * ACCELERATION -> acceleration += Coordinates(0, gravity.y)
-                abs(truncVelocity.y) > 5 * ACCELERATION -> acceleration += Coordinates(0, -gravity.y)
+                abs(nextVelocity.y) == 0L -> acceleration += Coordinates(0, 1)
+                abs(nextVelocity.y) < 2 * ACCELERATION -> acceleration += Coordinates(0, nextVelocity.y / abs(nextVelocity.y))
+                abs(nextVelocity.y) > 5 * ACCELERATION -> acceleration += Coordinates(0, -nextVelocity.y / abs(nextVelocity.y))
             }
 
             println("---------------------------")
@@ -38,6 +40,7 @@ class OrbitBot : AbstractBot() {
                     "position=$position " +
                     "nextPosition=$nextPosition " +
                     "velocity=$velocity " +
+                    "nextVelocity=$nextVelocity " +
                     "truncVelocity=$truncVelocity " +
                     "gravity=$gravity" +
                     "isCompensateX=$isCompensateX " +

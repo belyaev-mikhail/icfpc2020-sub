57891d77038e495c62cfdc9339707b20d6062dbf
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2020-07-19 19:40:49 +0300

    super orbit bot

--- app/icfpc2020/src/main/kotlin/ru/spbstu/sim/bot/BotUtil.kt
+++ app/icfpc2020/src/main/kotlin/ru/spbstu/sim/bot/BotUtil.kt
@@ -3,17 +3,18 @@ package ru.spbstu.sim.bot
 import ru.spbstu.sim.Coordinates
 import kotlin.math.abs
 
+const val ACCELERATION = 1L
 
 fun gravity(relTo: Coordinates): Coordinates {
     val (x, y) = relTo
     var res = Coordinates(0, 0)
-    if (x > 0 && abs(x) >= abs(y)) res += Coordinates(-1, 0)
-    if (x < 0 && abs(x) >= abs(y)) res += Coordinates(1, 0)
-    if (y > 0 && abs(x) <= abs(y)) res += Coordinates(0, -1)
-    if (y < 0 && abs(x) <= abs(y)) res += Coordinates(0, 1)
+    if (x > 0 && abs(x) >= abs(y)) res += Coordinates(-ACCELERATION, 0)
+    if (x < 0 && abs(x) >= abs(y)) res += Coordinates(ACCELERATION, 0)
+    if (y > 0 && abs(x) <= abs(y)) res += Coordinates(0, -ACCELERATION)
+    if (y < 0 && abs(x) <= abs(y)) res += Coordinates(0, ACCELERATION)
     return res
 }
 
-//fun abovePlant(relTo: Coordinates, planetDiameter: Long): Coordinates {
-//    val
-//}
+fun isAbovePlanet(relTo: Coordinates, planetRadius: Long): Boolean {
+    return abs(relTo.x) < planetRadius || abs(relTo.y) < planetRadius
+}

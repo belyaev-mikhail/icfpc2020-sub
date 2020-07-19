f98e562f2051c0a76e33c6f70804670bd0e519aa
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2020-07-20 00:06:06 +0300

    v1-2

--- app/icfpc2020/src/main/kotlin/ru/spbstu/sim/bot/OrbitBot.kt
+++ app/icfpc2020/src/main/kotlin/ru/spbstu/sim/bot/OrbitBot.kt
@@ -25,27 +25,27 @@ class OrbitBot : AbstractBot() {
             when {
                 isCompensateX || isCompensateXY -> acceleration += Coordinates(gravity.x, 0)
                 abs(nextVelocity.x) == 0L -> acceleration += Coordinates(1, 0)
-                abs(nextVelocity.x) < 2 * ACCELERATION -> acceleration += Coordinates(nextVelocity.x / abs(nextVelocity.x), 0)
-                abs(nextVelocity.x) > 5 * ACCELERATION -> acceleration += Coordinates(-nextVelocity.x / abs(nextVelocity.x), 0)
+                abs(nextVelocity.x) < 2 * ACCELERATION -> acceleration += Coordinates(-nextVelocity.x / abs(nextVelocity.x), 0)
+                abs(nextVelocity.x) > 5 * ACCELERATION -> acceleration += Coordinates(nextVelocity.x / abs(nextVelocity.x), 0)
             }
             when {
                 isCompensateY -> acceleration += Coordinates(0, gravity.y)
                 abs(nextVelocity.y) == 0L -> acceleration += Coordinates(0, 1)
-                abs(nextVelocity.y) < 2 * ACCELERATION -> acceleration += Coordinates(0, nextVelocity.y / abs(nextVelocity.y))
-                abs(nextVelocity.y) > 5 * ACCELERATION -> acceleration += Coordinates(0, -nextVelocity.y / abs(nextVelocity.y))
+                abs(nextVelocity.y) < 2 * ACCELERATION -> acceleration += Coordinates(0, -nextVelocity.y / abs(nextVelocity.y))
+                abs(nextVelocity.y) > 5 * ACCELERATION -> acceleration += Coordinates(0, nextVelocity.y / abs(nextVelocity.y))
             }
 
             println("---------------------------")
-            println("tick=${gameState.tick} " +
+            println("tick=${gameState.tick}\n" +
                     "position=$position " +
-                    "nextPosition=$nextPosition " +
+                    "nextPosition=$nextPosition\n" +
                     "velocity=$velocity " +
                     "nextVelocity=$nextVelocity " +
-                    "truncVelocity=$truncVelocity " +
-                    "gravity=$gravity" +
+                    "truncVelocity=$truncVelocity\n" +
+                    "gravity=$gravity\n" +
                     "isCompensateX=$isCompensateX " +
                     "isCompensateY=$isCompensateY " +
-                    "isCompensateXY=$isCompensateXY ")
+                    "isCompensateXY=$isCompensateXY")
 
             when {
                 acceleration.isZero() -> emptyList()

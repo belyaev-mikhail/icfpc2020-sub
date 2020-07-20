b715f42b9883b6374e8e9d5c2e5012cc75197b79
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2020-07-20 14:27:04 +0300

    orbit v2-1

--- app/icfpc2020/src/main/kotlin/ru/spbstu/sim/bot/OrbitBotV2.kt
+++ app/icfpc2020/src/main/kotlin/ru/spbstu/sim/bot/OrbitBotV2.kt
@@ -11,46 +11,65 @@ class OrbitBotV2 : AbstractBot() {
     init {
         step { ship, game, map, _ ->
             ArrayList<ShipCommand>().apply {
-                val orbitDuration = minOf(map.planeRadius, map.tickLimit - game.tick)
-                if (isDieAfter(orbitDuration, ship, map)) {
-                    add(ShipCommand.Accelerate(ship.id, -accelerate(ship)))
+                if (isDieAfter(minOf(map.planeRadius, map.tickLimit - game.tick), ship, map)) {
+                    println("${game.tick} DEAD DETECTED")
+                    add(ShipCommand.Accelerate(ship.id, accelerate(ship)))
+                } else if (isEscapeAfter(minOf(10, map.tickLimit - game.tick), ship, map)) {
+                    println("${game.tick} ESCAPE DETECTED")
                 }
             }
         }
     }
 
-    fun isDieAfter(ticks: Long, ship: GameShip, map: MapState): Boolean {
-        val planetRadius = map.planeRadius
+    private fun anyAfter(ticks: Long, ship: GameShip, check: (Coordinates) -> Boolean): Boolean {
         var position = ship.position
         var velocity = ship.velocity
-        if (isInsidePlanet(position, planetRadius)) return true
+        if (check(position)) return true
         for (i in 1..ticks) {
             position += velocity
             velocity -= gravity(position)
-            if (isInsidePlanet(position, planetRadius)) return true
+            if (check(position)) return true
         }
         return false
     }
 
-    fun isInsidePlanet(position: Coordinates, planetRadius: Long): Boolean {
-        return abs(position.x) <= planetRadius || abs(position.y) <= planetRadius
+    private fun isDieAfter(ticks: Long, ship: GameShip, map: MapState): Boolean {
+        val planetRadius = map.planeRadius
+        return anyAfter(ticks, ship) {
+            abs(it.x) <= planetRadius || abs(it.y) <= planetRadius
+        }
     }
 
-    fun accelerate(ship: GameShip): Coordinates {
+    private fun isEscapeAfter(ticks: Long, ship: GameShip, map: MapState): Boolean {
+        val spaceRadius = map.spaceRadius
+        return anyAfter(ticks, ship) {
+            abs(it.x) >= spaceRadius || abs(it.y) >= spaceRadius
+        }
+    }
+
+    private fun accelerate(ship: GameShip): Coordinates {
         val position = ship.position
         val velocity = ship.velocity
         val gravity = gravity(position)
 
+        println("DATA: position=$position, velocity=$velocity, gravity=$gravity")
+
         return when {
             gravity.x != 0L ->
                 when (velocity.y) {
-                    0L -> Coordinates(gravity.x, -position.y / abs(position.y))
-                    else -> Coordinates(gravity.x, -velocity.y / abs(velocity.y))
+                    0L -> when (position.y) {
+                        0L -> Coordinates(-gravity.x, 1)
+                        else -> Coordinates(-gravity.x, -position.y / abs(position.y))
+                    }
+                    else -> Coordinates(-gravity.x, -velocity.y / abs(velocity.y))
                 }
             gravity.y != 0L ->
                 when (velocity.x) {
-                    0L -> Coordinates(-position.x / abs(position.x), gravity.y)
-                    else -> Coordinates(-velocity.x / abs(velocity.x), gravity.y)
+                    0L -> when (position.x) {
+                        0L -> Coordinates(1, -gravity.y)
+                        else -> Coordinates(-position.x / abs(position.x), -gravity.y)
+                    }
+                    else -> Coordinates(-velocity.x / abs(velocity.x), -gravity.y)
                 }
             else -> TODO()
         }

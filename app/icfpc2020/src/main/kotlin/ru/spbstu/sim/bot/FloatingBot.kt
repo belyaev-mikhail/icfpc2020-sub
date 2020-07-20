bd4e1ecfa99abc64fa473de844368ae2f4595fc4
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2020-07-20 10:56:32 +0300

    test floating bot

---
+++ app/icfpc2020/src/main/kotlin/ru/spbstu/sim/bot/FloatingBot.kt
@@ -0,0 +1,31 @@
+package ru.spbstu.sim.bot
+
+import ru.spbstu.sim.MapState
+import ru.spbstu.sim.ShipCommand
+import ru.spbstu.sim.ShipState
+
+class FloatingBot : AbstractBot() {
+    override fun initialShipState(mapState: MapState) = ShipState(0, 0, 0, 0)
+    private var currentTurn = 0
+
+    init {
+        step { gameShip, gameState, mapState, list ->
+            ++currentTurn
+            if (currentTurn < 2) {
+                when {
+                    isAbovePlanet(gameShip.position, mapState.planeRadius) -> {
+                        val gravity = gravity(gameShip.position)
+                        val reverseGravity = gravity.swap()
+                        listOf(ShipCommand.Accelerate(gameShip.id, reverseGravity * 2 + gravity))
+                    }
+                    else -> {
+                        val acceleration = gravity(gameShip.position) * 2
+                        listOf(ShipCommand.Accelerate(gameShip.id, acceleration))
+                    }
+                }
+            } else {
+                listOf()
+            }
+        }
+    }
+}

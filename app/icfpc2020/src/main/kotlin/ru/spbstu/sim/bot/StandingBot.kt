e48354e7ae633c0c81d757872c886c00069d5885
Marat Akhin <marat.akhin@gmail.com>
2020-07-20 10:35:40 +0300

    Standing bot tried to run away from kamikazes fixed

--- app/icfpc2020/src/main/kotlin/ru/spbstu/sim/bot/StandingBot.kt
+++ app/icfpc2020/src/main/kotlin/ru/spbstu/sim/bot/StandingBot.kt
@@ -19,38 +19,38 @@ class StandingBot : AbstractBot() {
     init {
         step { ship, state, map, _ ->
 
+            fun default(): List<ShipCommand> {
+                val velocity = gravity(ship.position)
+                return listOf(ShipCommand.Accelerate(ship.id, velocity + velocity.randomVector))
+            }
+
             val sim = OrbitSim(Planet(map.planeRadius.toInt()), state.ships.filter { it.role != ship.role })
             sim.tick(1)
 
             val kamikaze = sim.ships.entries.filter { (id, e) ->
                 ship.position.manhattanDist(e.coords) <= state.shipMap[id]!!.explosionRadius
-            }.minBy { (_, e) -> ship.position.manhattanDist(e.coords) }
+            }.minBy { (_, e) -> ship.position.manhattanDist(e.coords) } ?: return@step default()
 
-            if (kamikaze == null) {
-                val velocity = gravity(ship.position)
-                return@step listOf(ShipCommand.Accelerate(ship.id, velocity + velocity.randomVector))
-            }
-
-            var target = ship.position + (ship.position - kamikaze.value.coords)
+            val idealTarget = ship.position + (ship.position - kamikaze.value.coords)
+            var target = idealTarget
 
             if (target.manhattanDist(Coordinates(0, 0)) <= map.planeRadius) {
-                target = ship.position + (ship.position - kamikaze.value.coords)
-                target = target.copy(x = ship.position.x)
+                target = idealTarget.copy(x = ship.position.x)
             }
             if (target.manhattanDist(Coordinates(0, 0)) <= map.planeRadius) {
-                target = ship.position + (ship.position - kamikaze.value.coords)
-                target = target.copy(y = ship.position.y)
+                target = idealTarget.copy(y = ship.position.y)
             }
             if (target.manhattanDist(Coordinates(0, 0)) <= map.planeRadius) {
-                val velocity = gravity(ship.position)
-                return@step listOf(ShipCommand.Accelerate(ship.id, velocity + velocity.randomVector))
+                return@step default()
             }
 
-            val path = sim.findPath(ship.id, target) ?: return@step listOf()
+            val sim2 = OrbitSim(Planet(map.planeRadius.toInt()), state.ships.filter { it.id == ship.id })
+
+            val path = sim2.findPath(ship.id, target) ?: return@step listOf()
             val pp = path.reversed()
             val accel = pp[1].v - pp[0].v
 
-            if (!accel.isZero()) listOf() else listOf(ShipCommand.Accelerate(ship.id, accel))
+            if (accel.isZero()) listOf() else listOf(ShipCommand.Accelerate(ship.id, accel))
         }
     }
 }
